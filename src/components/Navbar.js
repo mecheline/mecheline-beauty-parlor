bcc71141303ec0b59f3bc6e9cd87960db2e5c3bd
mecheline <ucheamaechi2016@gmail.com>
2020-10-22 23:18:37 +0100

    navbar fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,34 +9,37 @@ const Navbar = () => {
     },[<Link to = '/'></Link>, <Link to = '/about'></Link>])
     
     return (
-        <div className='nav-index'>
-            <div className='top'>
-                <h4 className='parlor insta'>Mecheline Beauty Parlor</h4>
-                <h5 className='experience insta'>It's a wonderful experience...</h5>
-            </div>
-            <div>
-                <nav className='pushpin'>
-                    <div className="nav-wrapper navColor">
-                    <div className='container'>
-                    <Link to="/" className="brand-logo insta">Mechelin Hair Cuts</Link>
-                    <Link to="#" data-target="mobile-demo" className="sidenav-trigger"><i class="material-icons">menu</i></Link>
-                        <ul id="nav-mobile" className="right hide-on-med-and-down">
-                            <li><Link to="/">Home</Link></li>
-                            <li><Link to="/about">About us</Link></li>
-                            <li><Link to="/contacts">Contacts</Link></li>
-                            <li><Link to="/services">Services</Link></li>
-                        </ul>
-                    </div>
-                    </div>
-                </nav>
-            </div>
+        <div>
+             <div className='nav-index'>
+                <div className='navbar-fixed'>
+                    <nav className='nav-menu'>
+                        <div className="nav-wrapper">
+                        <div className='container'>
+                        <Link to="/" className="brand-logo insta">Mechelin Hair Cuts</Link>
+                        <Link to="#" data-target="mobile-demo" className="sidenav-trigger"><i class="material-icons">menu</i></Link>
+                            <ul id="nav-mobile" className="right hide-on-med-and-down">
+                                <li><Link to="/">Home</Link></li>
+                                <li><Link to="/about">About us</Link></li>
+                                <li><Link to="/contacts">Contacts</Link></li>
+                                <li><Link to="/services">Services</Link></li>
+                            </ul>
+                        </div>
+                        </div>
+                    </nav>
+                </div>
 
-            <ul class="sidenav" id="mobile-demo">
-                <li><Link className='sidenav-close' to="/">Home</Link></li>
-                <li><Link className='sidenav-close' to="/about">About us</Link></li>
-                <li><Link className='sidenav-close' to="/contacts">Contacts</Link></li>
-                <li><Link className='sidenav-close' to="/services">Services</Link></li>
-            </ul>
+                <ul class="sidenav" id="mobile-demo">
+                    <li><Link className='sidenav-close' to="/">Home</Link></li>
+                    <li><Link className='sidenav-close' to="/about">About us</Link></li>
+                    <li><Link className='sidenav-close' to="/contacts">Contacts</Link></li>
+                    <li><Link className='sidenav-close' to="/services">Services</Link></li>
+                </ul>
+
+                <div className='top'>
+                    <h4 className='parlor insta'>Mecheline Beauty Parlor</h4>
+                    <h5 className='experience insta'>It's a wonderful experience...</h5>
+                </div>
+            </div>
         </div>
     );
 };
--- src/screens/ContactPage.js
+++ src/screens/ContactPage.js
@@ -4,14 +4,14 @@ const ContactPage = () => {
     return (
         <div className='container'>
             <div className="row">
-                <div className="col offset-s4 s9 offset-m4 m8 offset-l2 l5 contact melonga">
+                <div className="col s12 offset-m4 m4 offset-l4 l4 contact melonga">
                     
                     <h5>Book an Appointment</h5>
                 </div>
             </div>
             <div className="row">
-                <div className="col  frame">
-                    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSd9CzD132XTnEK9ZjUUdA9QyP5AETkd7YBp1Il9ltZjnDjWww/viewform?embedded=true" width="640" height="1679" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+                <div className="col s6 offset-m4 m4 offset-l4 l4 frame">
+                    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSd9CzD132XTnEK9ZjUUdA9QyP5AETkd7YBp1Il9ltZjnDjWww/viewform?embedded=true" width="400" height="1600" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
                 </div>   
             </div>
                
