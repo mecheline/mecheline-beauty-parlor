e85245dab2bfb0867c8c27a1dc08d38a529836fa
mecheline <ucheamaechi2016@gmail.com>
2021-01-11 15:07:54 +0100

    google edit

--- src/screens/ContactPage.js
+++ src/screens/ContactPage.js
@@ -10,8 +10,8 @@ const ContactPage = () => {
                 </div>
             </div>
             <div className="row">
-                <div className="col s6 offset-m4 m4 offset-l4 l4 frame">
-                    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSd9CzD132XTnEK9ZjUUdA9QyP5AETkd7YBp1Il9ltZjnDjWww/viewform?embedded=true" width="400" height="1600" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+                <div className="">
+                    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSd9CzD132XTnEK9ZjUUdA9QyP5AETkd7YBp1Il9ltZjnDjWww/viewform?embedded=true" width="700" height="600" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
                 </div>   
             </div>
                
